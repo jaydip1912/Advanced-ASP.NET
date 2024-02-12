9a1061c3a8820e1bf681ec7126db18d93d081b65
jaydip patel <jaydippatel.jp275@gmail.com>
2024-02-12 23:59:43 +0530

    First

---
+++ StoreProcedure/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace StoreProcedure
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                GetProductList();
+            }
+        }
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jaydi\\OneDrive\\Documents\\Crud.mdf;Integrated Security=True;Connect Timeout=30;");
+        protected void btninsert_Click(object sender, EventArgs e)
+        {
+            int productid=int.Parse(TextBox1.Text);
+            string iname =TextBox2.Text,specification =TextBox3.Text,unit=DropDownList1.SelectedValue, status=RadioButtonList1.SelectedValue ;
+            DateTime dateTime = DateTime.Parse(TextBox4.Text);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("exec ProductSetup '" + productid +"','"+iname+"','"+specification+"','"+unit+"','"+status+"','"+dateTime+"'",con);
+            cmd.ExecuteNonQuery(); 
+            ScriptManager.RegisterStartupScript(this,this.GetType(),"script","alert('insert');",true);
+            GetProductList();
+            
+        }
+        void GetProductList()
+        {
+            
+            SqlCommand cd= new SqlCommand("exec ProductList ", con);
+            SqlDataAdapter sd= new SqlDataAdapter(cd);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
